3c4956bd3b92ba449e94bd4710fce605e90d8c4d
Biswarup Jana <biswaruprx21@gmail.com>
2023-04-01 12:20:35 +0530

    #

--- redux_in_react/project_2/src/store/cart-slice.js
+++ redux_in_react/project_2/src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { uiActions } from './ui-slice';
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -37,6 +38,55 @@ const cartSlice = createSlice({
   },
 });
 
-export const cartActions = cartSlice.actions;
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: 'pending',
+        title: 'Sending..',
+        message: 'Sending cart data!',
+      })
+    );
 
-export default cartSlice;
+    const sendRequest = async () => {
+
+      const response = await fetch(
+        'https://react-https-b5b53-default-rtdb.firebaseio.com/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart),
+        });
+      if (!response.ok) {
+        throw new Error('Sending cart data failed.');
+      }
+    };
+    try {
+
+      await sendRequest();
+
+      dispatch(
+        uiActions.showNotification({
+          status: 'Success',
+          title: 'Success!',
+          message: 'Sent cart data successfully!',
+        }))
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Sending cart data failed!',
+        })
+      );
+    }
+
+    // const sendCartData = async () => {
+
+    // }
+  };
+}
+
+
+  export const cartActions = cartSlice.actions;
+
+  export default cartSlice;
